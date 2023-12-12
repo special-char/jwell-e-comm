e42372e884c274ea0559ddfbdbd4a0ed87db5c2d
dharmikpatel21 <dharmikjpatel21@gmail.com>
2023-12-12 20:35:33 +0530

    product card alomost completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,31 @@ import Banner from "@/components/common/Banner";
 import TestimonialSection from "@/containers/TestimonialSection";
 import WhyChooseUsSection from "@/containers/WhyChooseUs";
 import Link from "next/link";
-import { homePageBanner } from "@/lib/data";
+import { homePageBanner, products } from "@/lib/data";
 import Footer from "@/components/common/Footer";
+import ProductCard from "@/components/common/ProductCard";
 
 export default function Home() {
-	return (
-		<main className="flex flex-col gap-2">
-			<h2>Normal Routes</h2>
-			<Link href="/products">shop or all products</Link>
-			<Link href="/products/p001">product/p001</Link>
-			<Link href="/about-us">About us</Link>
-			<Link href="/contact-us">Contact-us</Link>
-			<h2>Intercepting Routes</h2>
-			<Link href={`/login`}>login</Link>
-			<Link href={`/cart`}>Open cart</Link>
-			<Link href="/products/p001">product/p001</Link>
-			<WhyChooseUsSection />
-			<TestimonialSection />
-			<Banner data={homePageBanner} />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className="flex flex-col gap-2">
+      <div className="flex">
+        <ProductCard data={products[0]} />
+        <ProductCard data={products[1]} />
+        <ProductCard data={products[2]} />
+      </div>
+      <h2>Normal Routes</h2>
+      <Link href="/products">shop or all products</Link>
+      <Link href="/products/p001">product/p001</Link>
+      <Link href="/about-us">About us</Link>
+      <Link href="/contact-us">Contact-us</Link>
+      <h2>Intercepting Routes</h2>
+      <Link href={`/login`}>login</Link>
+      <Link href={`/cart`}>Open cart</Link>
+      <Link href="/products/p001">product/p001</Link>
+      <WhyChooseUsSection />
+      <TestimonialSection />
+      <Banner data={homePageBanner} />
+      <Footer />
+    </main>
+  );
 }
--- src/components/common/ProductCard/index.tsx
+++ src/components/common/ProductCard/index.tsx
@@ -1,7 +1,46 @@
-type Props = {};
+import React from "react";
+import { products } from "@/lib/data";
+import Image from "next/image";
+import Styles from "./productcard.module.css";
+import Button from "../Button";
+type Props = {
+  data: {
+    id: number;
+    imgUrl: string;
+    title: string;
+    price: string;
+    btn: string;
+    desription: string;
+  };
+};
 
-const ProductCard = (props: Props) => {
-	return <div>ProductCard</div>;
+const ProductCard = ({ data }: Props) => {
+  const Icon = data.btn;
+
+  return (
+    <div
+      key={data.id}
+      className={`flex flex-col gap-4 items-center py-5 ${Styles.main}`}
+    >
+      <div className="aspect-square w-[250px] relative">
+        <Image
+          src={data.imgUrl}
+          alt={data.title}
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <p>{data.title}</p>
+        <p className="font-bold">{data.price}</p>
+      </div>
+      <div className="absolute -bottom-5 ">
+        <Button variant="round" color="black">
+          <Icon className={"h-4"} />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
