465a58c5b9c4aba62f7d0ac5c2cc538e5be07c90
Jay Kanjia <jaykanjia11@gmail.com>
2023-12-13 16:29:46 +0530

    header updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import HomeBanner from "@/containers/HomeBanner";
 export default function Home() {
 	return (
 		<main>
-			<Header />́
+			<Header />
 			<HomeBanner />
 			<WhyChooseUsSection />
 			<TestimonialSection />
