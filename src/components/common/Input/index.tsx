b2294ac6f550054e719682b752eded0e4ef69318
Aaryan Patel <aaryan_20061@ldrp.ac.in>
2023-12-13 16:13:12 +0530

    "input component created"

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { forwardRef, memo } from "react";
+import style from "./input.module.css";
+import clsx from "clsx";
 
-type Props = {};
-
-const Input = (props: Props) => {
-  return <div>Input</div>;
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  color?: "white";
+  // variant?: "fill" | "outline";
+  className?: any;
+  label?: string;
+};
+const Input = ({
+  name,
+  color,
+  // variant = "fill",
+  label,
+  className,
+  ...props
+}: Props) => {
+  return (
+    <div className={style.input_group}>
+      {label && (
+        <label
+          htmlFor={name}
+          className={clsx(style.label, {
+            [style.label_required]: props.required,
+          })}
+        >
+          {label}
+        </label>
+      )}
+      <span className={clsx(style.input_wrapper)}>
+        <input
+          className={clsx(style.input, "peer/input", {
+            [style[`input--${color}`]]: !!color,
+            // [style[`input--${variant}`]]: !!variant,
+            [className || ""]: !!className,
+          })}
+          {...props}
+        />
+      </span>
+    </div>
+  );
 };
 
-export default Input;
+export default memo(Input);
