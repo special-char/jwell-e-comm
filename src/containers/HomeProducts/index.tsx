980f2df19e60688cfc3d79468af276f81af341f4
Jay Kanjia <jaykanjia11@gmail.com>
2023-12-13 22:47:55 +0530

    ui bug fixes

--- src/containers/HomeProducts/index.tsx
+++ src/containers/HomeProducts/index.tsx
@@ -8,13 +8,11 @@ const HomeProducts = (props: Props) => {
 	return (
 		<section className="grid lg:grid-cols-[25%_1fr] grid-cols-1 items-center gap-2">
 			<div className="flex flex-col gap-4">
-				<h3>{topProduct.heading}</h3>
-				<p className="text-caption1">{topProduct.description}</p>
-				<div>
-					<Button variant="fill" color="black">
-						{topProduct.actionBtn}
-					</Button>
-				</div>
+				<h5 className="font-semibold">{topProduct.heading}</h5>
+				<p className="text-caption2 text-gray mb-2">{topProduct.description}</p>
+				<Button variant="fill" color="black" className="w-max">
+					{topProduct.actionBtn}
+				</Button>
 			</div>
 			<div className="grid md:grid-cols-3 grid-cols-1 gap-4">
 				{products.slice(0, 3).map((product, index) => (
