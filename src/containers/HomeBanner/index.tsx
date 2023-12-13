a2e299b6007929cfafebab17da04a2a95652cefb
Jay Kanjia <jaykanjia11@gmail.com>
2023-12-13 12:25:21 +0530

    home banner section

---
+++ src/containers/HomeBanner/index.tsx
@@ -0,0 +1,8 @@
+import Banner from "@/components/common/Banner";
+import { homePageBanner } from "@/lib/data";
+
+const HomeBanner = () => {
+	return <Banner data={homePageBanner} />;
+};
+
+export default HomeBanner;
