2b4002585d440cad8bc09d27c4123551351515bd
Jay Kanjia <jaykanjia11@gmail.com>
2023-12-13 18:29:35 +0530

    testimonial card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import HomeBanner from "@/containers/HomeBanner";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialSection from "@/containers/TestimonialSection";
-import ProductCard from "@/components/common/ProductCard";
-import { products } from "@/lib/data";
+import HomeProducts from "@/containers/HomeProducts";
 
 export default function Home() {
-  return (
-    <main>
-      <HomeBanner />
-      <WhyChooseUs />
-      <TestimonialSection />
-      <ProductCard data={products[0]} />
-    </main>
-  );
+	return (
+		<main>
+			<HomeBanner />
+			<HomeProducts />
+			<WhyChooseUs />
+			<TestimonialSection />
+		</main>
+	);
 }
