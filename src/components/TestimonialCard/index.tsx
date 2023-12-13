8516cd1045f98c83ec2973487bacf3fae9429f05
Jay Kanjia <jaykanjia11@gmail.com>
2023-12-13 22:04:24 +0530

    module css testimonial footer

--- src/components/TestimonialCard/index.tsx
+++ src/components/TestimonialCard/index.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import { testimonialCardType } from "@/utils/types";
+import styles from "./testimonialCard.module.css";
 
 const TestimonialCard = ({ data }: testimonialCardType) => {
-  return (
-    <div className="sm:w-[60%] mx-auto flex flex-col justify-center items-center gap-10 pb-20">
-      <p className="text-center text-gray">{data.description}</p>
-      <div className="flex flex-col gap-2 items-center">
-        <div className="w-20 aspect-square relative rounded-full overflow-hidden">
-          <Image src={data.image} alt="testimonial person" fill />
-        </div>
-        <p className="small font-bold">{data.name}</p>
-        <p className="small text-gray">{data.designation}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.testimonialCard}>
+			<p className={styles.testimonialDescription}>{data.description}</p>
+			<div className={styles.testimonialWrapper}>
+				<div className={styles.testimonialPersonImage}>
+					<Image src={data.image} alt="testimonial person" fill />
+				</div>
+				<p className={styles.testimonialPersonName}>{data.name}</p>
+				<p className={styles.testimonialPersonDesignation}>
+					{data.designation}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default TestimonialCard;
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -6,24 +6,25 @@ import SocialIcons from "@/components/common/SocialIcons";
 import SendIcon from "@/public/icons/send.svg";
 import Input from "../Input";
 import Image from "next/image";
+import styles from "./footer.module.css";
 
 type Props = {};
 
 const Footer = (props: Props) => {
 	return (
-		<div className="bg-white relative max-md:mt-20 mt-40 pt-20">
-			<div className="absolute right-0 top-0 min-w-[200px] w-[30%] max-w-[350px] aspect-square -translate-y-[40%] -translate-x-[10%]">
-				<Image fill src="/images/footerImage.png" alt="footer image" />
+		<div className={styles.footerWrapper}>
+			<div className={styles.footerImageWrapper}>
+				<Image fill src={footerData.footerImage} alt="footer image" />
 			</div>
 			{/* newsletter */}
-			<section className="pb-0">
-				<div className="flex gap-2 items-center">
-					<MailIcon className="w-8 aspect-square" />
-					<p className="font-semibold text-green">
+			<section>
+				<div className={styles.newsLetterHeadingWrapper}>
+					<MailIcon className={styles.newsLetterHeadingIcon} />
+					<p className={styles.newsLetterHeading}>
 						{footerData.newsLetter.heading}
 					</p>
 				</div>
-				<form className="flex gap-4 items-center flex-wrap">
+				<form className={styles.newsLetterForm}>
 					<Input name="name" placeholder="Enter your name" />
 					<Input name="email" placeholder="Enter your email" type="email" />
 					<Button
@@ -32,27 +33,27 @@ const Footer = (props: Props) => {
 						className="!rounded-xl"
 						type="submit"
 					>
-						<SendIcon className="w-6 aspect-square" />
+						<SendIcon className={styles.newsLetterFormIcon} />
 					</Button>
 				</form>
 			</section>
-			<footer className="py-20 flex flex-col gap-4">
-				<h4 className="text-green font-semibold">{footerData.heading}</h4>
-				<div className="grid lg:grid-cols-[30%_1fr] gap-8 mb-10">
-					<div className="flex flex-col gap-4">
-						<p className="small text-gray">{footerData.description}</p>
+			<footer className={styles.footer}>
+				<h4 className={styles.heading}>{footerData.heading}</h4>
+				<div className={styles.footerDataWrapper}>
+					<div className={styles.descriptionWrapper}>
+						<p className={styles.description}>{footerData.description}</p>
 						<SocialIcons />
 					</div>
-					<div className="grid grid-cols-2 md:grid-cols-4 gap-4 gap-y-10">
+					<div className={styles.footerLinks}>
 						{footerData?.footerLinks?.map((item, index) => {
 							return (
-								<div key={index} className="flex flex-col gap-4">
+								<div key={index} className={styles.footerLinkSection}>
 									{item?.map((link) => {
 										return (
 											<Link
 												key={link.id}
 												href={link.url}
-												className="hover:text-gray/70 duration-300"
+												className={styles.link}
 											>
 												{link.title}
 											</Link>
@@ -64,13 +65,20 @@ const Footer = (props: Props) => {
 					</div>
 				</div>
 				<hr className="opacity-30" />
-				<div className="flex max-md:flex-col md:justify-between gap-4 text-gray">
-					<p className="text-center sm">
-						Copyright ©2023. All Rights Reserved.
-					</p>
-					<div className="flex gap-4 justify-center items-center text-caption2">
-						<span>Terms & Conditions</span>
-						<span>Privacy Policy</span>
+				<div className={styles.copyrightWrapper}>
+					<p className="sm">{footerData.copyright.description}</p>
+					<div className={styles.copyrightLinks}>
+						{footerData.copyright.links.map((link) => {
+							return (
+								<Link
+									key={link.id}
+									href={link.url}
+									className={styles.copyrightLink}
+								>
+									{link.title}
+								</Link>
+							);
+						})}
 					</div>
 				</div>
 			</footer>
