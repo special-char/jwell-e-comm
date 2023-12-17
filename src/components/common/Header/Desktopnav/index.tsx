bf54671ad01d703cffb41aa7bd19ed663d34faa9
dharmikpatel21 <dharmikjpatel21@gmail.com>
2023-12-17 16:29:34 +0530

    link added in main logo for home

--- src/components/common/Header/Desktopnav/index.tsx
+++ src/components/common/Header/Desktopnav/index.tsx
@@ -8,48 +8,48 @@ import { usePathname } from "next/navigation";
 import AccountBtn from "../AccountBtn";
 
 const DesktopNav = ({ data }: DesktopNavType) => {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<div className={style.deskNav}>
-			<div className={style.logoWrapper}>
-				<h1 className={style.heading1}>{data.heading1}</h1>
-				<h1 className={style.heading2}>{data.heading2}</h1>
-			</div>
-			<div className={style.contentWrapper}>
-				<div className={style.navLinks}>
-					{data &&
-						data.navLinks.map((item) => {
-							return (
-								// <Link href={item.url} key={item.id} className={style.title}>
-								//   {item.title}
-								// </Link>
-								<Link
-									href={item.url}
-									key={item.id}
-									className={clsx(`${style.link}`, {
-										[style.active]: pathname === item.url,
-									})}
-								>
-									{item.title}
-								</Link>
-							);
-						})}
-				</div>
-				<div className={style.icons}>
-					{/* <Link href={"/account"}>
+  return (
+    <div className={style.deskNav}>
+      <Link href={"/"} className={style.logoWrapper}>
+        <h1 className={style.heading1}>{data.heading1}</h1>
+        <h1 className={style.heading2}>{data.heading2}</h1>
+      </Link>
+      <div className={style.contentWrapper}>
+        <div className={style.navLinks}>
+          {data &&
+            data.navLinks.map((item) => {
+              return (
+                // <Link href={item.url} key={item.id} className={style.title}>
+                //   {item.title}
+                // </Link>
+                <Link
+                  href={item.url}
+                  key={item.id}
+                  className={clsx(`${style.link}`, {
+                    [style.active]: pathname === item.url,
+                  })}
+                >
+                  {item.title}
+                </Link>
+              );
+            })}
+        </div>
+        <div className={style.icons}>
+          {/* <Link href={"/account"}>
 						<span className="sr-only">user account button</span>
 						<UserIcon className="h-5" />
 					</Link> */}
-					<AccountBtn />
-					<Link href={"/cart"} scroll={false}>
-						<span className="sr-only">go to cart button</span>
-						<CartIcon className="h-5" />
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+          <AccountBtn />
+          <Link href={"/cart"} scroll={false}>
+            <span className="sr-only">go to cart button</span>
+            <CartIcon className="h-5" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DesktopNav;
